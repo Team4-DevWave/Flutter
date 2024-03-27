8914e96625bd02d8c4143fd7a14e44b0f76c91e7
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2024-03-28 00:50:22 +0200

    Phase 3 initial Point

--- threddit_app/lib/features/user_system/model/user_data.dart
+++
@@ -1,4 +0,0 @@
-Map<String, String> authData = {
-  'email': '',
-  'password': '',
-};
--- threddit_app/lib/features/user_system/view_model/auth_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class Auth with ChangeNotifier {
-  late String _token;
-  late DateTime _expiryDate;
-  late String _userId;
-
-  Future<void> signup(String email, String psasword) async {}
-}
