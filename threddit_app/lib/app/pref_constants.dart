6eee3d9da7ca0b2d9b7865a00f0b3687ba915222
Doha ElBeltagy <doha.elbeltagy03@eng-st.cu.edu.eg>
2024-04-09 17:07:30 +0200

    UserScreen fixation + drawers fixation

---
+++ threddit_app/lib/app/pref_constants.dart
@@ -0,0 +1,5 @@
+class PrefConstants {
+  static String favourites = 'favourites';
+  static String googleToken = 'googleToken';
+  static String authToken = 'authToken';
+}
--- threddit_app/lib/features/home_page/view/widgets/left_drawer.dart
+++ threddit_app/lib/features/home_page/view/widgets/left_drawer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:threddit_clone/features/home_page/view/widgets/communities_tiles.dart';
 import 'package:threddit_clone/features/home_page/view/widgets/following_tiles.dart';
 import 'package:threddit_clone/theme/colors.dart';
@@ -7,38 +7,44 @@ import 'package:threddit_clone/theme/text_styles.dart';
 
 ///TODO: IMPLEMENT THE FAVOURITES LIST AND RECENTLY VIEWED USING SHARED PREFERENCES.
 
-class LeftDrawer extends ConsumerStatefulWidget{
+class LeftDrawer extends StatefulWidget{
   const LeftDrawer({super.key});
+
   @override
-  ConsumerState<LeftDrawer> createState()=>  _LeftDrawerState();
+  State<LeftDrawer> createState() => _LeftDrawerState();
 }
 
-class _LeftDrawerState extends ConsumerState<LeftDrawer>{
+class _LeftDrawerState extends State<LeftDrawer> {
+  String?_favourites;
+
+  void _getFavourites() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _favourites = prefs.getString('favourites');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Theme(
-        data: Theme.of(context).copyWith(canvasColor: AppColors.mainColor),
-        child: Drawer(
-          elevation: double.maxFinite,
-          backgroundColor: AppColors.mainColor,
-          shadowColor: AppColors.mainColor,
-          surfaceTintColor: AppColors.mainColor,
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Text(
-                  'Recently Visited',
-                  style: AppTextStyles.primaryTextStyle
-                      .copyWith(fontWeight: FontWeight.bold),
-                ),
-              ),
-              const CommunitiesTiles(title: "Communities"),
-              const FollowingTiles(title: "Following"),
-            ],
+    return Drawer(
+      elevation: double.maxFinite,
+      backgroundColor: AppColors.mainColor,
+      shadowColor: AppColors.mainColor,
+      surfaceTintColor: AppColors.mainColor,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Recently Visited',
+              style: AppTextStyles.primaryTextStyle
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
-      );
+          const CommunitiesTiles(title: "Communities"),
+          const FollowingTiles(title: "Following"),
+        ],
+      ),
+    );
   }
 }
