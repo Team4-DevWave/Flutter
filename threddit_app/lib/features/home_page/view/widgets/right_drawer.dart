481674f960bb6e8d46e962851626c7a7db970e93
sh3boly <117195948+sh3boly@users.noreply.github.com>
2024-04-11 00:12:42 +0200

    Correct routing instead of one used for testing

--- threddit_app/lib/features/home_page/view/widgets/right_drawer.dart
+++ threddit_app/lib/features/home_page/view/widgets/right_drawer.dart
@@ -90,7 +90,7 @@ class RightDrawer extends ConsumerWidget{
                 ),
                 title: "Settings",
                 onTap: () {
-                  Navigator.pushNamed(context, RouteClass.communityModTools);
+                  Navigator.pushNamed(context, RouteClass.settingsScreen);
                 }),
           ],
         ),
