69b0e220a2b151e269da2f97f546c627b2383212
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2024-04-13 03:10:17 +0200

    adding video to feed

--- threddit_app/lib/features/home_page/view/screens/home_screen.dart
+++ threddit_app/lib/features/home_page/view/screens/home_screen.dart
@@ -5,7 +5,6 @@ import 'package:threddit_clone/app/route.dart';
 import 'package:threddit_clone/features/home_page/view/widgets/left_drawer.dart';
 import 'package:threddit_clone/features/home_page/view/widgets/right_drawer.dart';
 import 'package:threddit_clone/features/listing/view/widgets/feed_widget.dart';
-import 'package:threddit_clone/features/user_system/model/token_storage.dart';
 import 'package:threddit_clone/theme/colors.dart';
 import 'package:threddit_clone/theme/text_styles.dart';
 
@@ -31,7 +30,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   }
 
   Future<void> getUserName() async {
-    userId = await getUserID();
+    //userId = await getUserID();
     print("RRRRRRRRRRRRRRRRRRRRRRRRR");
     print(userId);
   }
