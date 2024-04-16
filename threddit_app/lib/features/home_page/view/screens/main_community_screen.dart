c32ed590a4e423c7c3d1c0764bf5886effa0b0b0
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2024-04-17 01:16:10 +0200

    Before Deploying

--- threddit_app/lib/features/home_page/view/screens/main_community_screen.dart
+++ threddit_app/lib/features/home_page/view/screens/main_community_screen.dart
@@ -1,20 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lottie/lottie.dart';
+import 'package:threddit_clone/features/community/view%20model/get_community.dart';
+import 'package:threddit_clone/features/home_page/view/widgets/community_feed_unit.dart';
+import 'package:threddit_clone/features/user_system/model/token_storage.dart';
+
+import 'package:threddit_clone/models/subreddit.dart';
+import 'package:threddit_clone/theme/colors.dart';
+import 'package:threddit_clone/theme/text_styles.dart';
 
 class MainCommunityScreen extends ConsumerStatefulWidget {
   const MainCommunityScreen({super.key});
+
   @override
   ConsumerState<MainCommunityScreen> createState() => _CommunityScreenState();
 }
 
 class _CommunityScreenState extends ConsumerState<MainCommunityScreen> {
+  late Future<SubredditList> futuredata;
+  late String userId;
+  @override
+  void initState() {
+    futuredata = fetchSubredditsAll();
+    setUserid();
+    super.initState();
+  }
+
+  Future<void> setUserid() async {
+    userId = (await getUserId())!;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center();
-    // child: ElevatedButton(
-    //     onPressed: () {
-    //       share(context, ref, posts[0]);
-    //     },
-    //     child: Text('Share')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Communities",
+          style: AppTextStyles.boldTextStyle,
+        ),
+      ),
+      body: Container(
+          color: AppColors.backgroundColor,
+          child: FutureBuilder<SubredditList>(
+            future: futuredata,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: Lottie.asset(
+                    'assets/animation/loading.json',
+                    repeat: true,
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              } else if (snapshot.hasData) {
+                final subredditData = snapshot.data!.subreddits;
+
+                return ListView.builder(
+                  itemCount: subredditData.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CommunityUnit(
+                      subreddit: subredditData[index],
+                      userID: userId,
+                    );
+                  },
+                );
+              } else {
+                return const Text('No data available.');
+              }
+            },
+          )),
+    );
   }
 }
---
+++ threddit_app/lib/features/home_page/view/widgets/community_feed_unit.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:threddit_clone/app/route.dart';
+import 'package:threddit_clone/models/subreddit.dart';
+import 'package:threddit_clone/theme/colors.dart';
+import 'package:threddit_clone/theme/text_styles.dart';
+
+class CommunityUnit extends StatefulWidget {
+  final Subreddit subreddit;
+  final String userID;
+  CommunityUnit({super.key, required this.subreddit, required this.userID});
+
+  @override
+  State<CommunityUnit> createState() => _CommunityUnitState();
+}
+
+class _CommunityUnitState extends State<CommunityUnit> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipOval(
+                    child: widget.subreddit.srLooks.icon != ''
+                        ? Image.network(
+                            widget.subreddit.srLooks.icon,
+
+                            fit: BoxFit.cover,
+                            width: 45
+                                .w, // You can adjust width and height to your needs
+                            height: 45.h,
+                          )
+                        : SizedBox(
+                            width: 45,
+                            height: 45.h,
+                            child: Image.asset(
+                              'assets/images/Reddit_Icon_FullColor.png',
+                            ),
+                          ),
+                  ),
+                  SizedBox(
+                    width: 15.w,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(widget.subreddit.name,
+                          style: AppTextStyles.primaryButtonGlowTextStyle),
+                      Text(
+                        "${widget.subreddit.members.length} members",
+                        style: AppTextStyles.secondaryTextStyle,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: AppColors.redColor,
+                  borderRadius:
+                      BorderRadius.circular(40), // Adjust the radius as needed
+                ),
+                child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, RouteClass.communityScreen,
+                          arguments: {
+                            'id': widget.subreddit.name,
+                            'uid': widget.userID
+                          });
+                    },
+                    child: Text(
+                      "Visit",
+                      style: AppTextStyles.primaryButtonGlowTextStyle,
+                    )),
+              )
+            ],
+          ),
+          SizedBox(height: 10.h),
+          Container(
+            padding: EdgeInsets.only(left: 60.w),
+            child: Text(
+              maxLines: 2,
+              widget.subreddit.status,
+              style: AppTextStyles.secondaryTextStyle,
+            ),
+          ),
+          Divider(
+            color: Color.fromRGBO(233, 93, 95, 0.573),
+            thickness: 1,
+          )
+        ],
+      ),
+    );
+  }
+}
