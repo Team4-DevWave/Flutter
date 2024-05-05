71ab56e51dc4809e609adaed82df4e352e1ba2aa
sh3boly <117195948+sh3boly@users.noreply.github.com>
2024-05-05 20:13:58 +0300

    Add sorting to Search function

--- threddit_app/lib/features/searching/view/widgets/search_community_widget.dart
+++ threddit_app/lib/features/searching/view/widgets/search_community_widget.dart
@@ -4,10 +4,12 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lottie/lottie.dart';
 import 'package:threddit_clone/features/home_page/view/widgets/community_feed_unit.dart';
 import 'package:threddit_clone/features/searching/model/search_model.dart';
+import 'package:threddit_clone/features/searching/view/widgets/search_feed_widget.dart';
 import 'package:threddit_clone/features/searching/view_model/searching_apis.dart';
 import 'package:threddit_clone/features/user_system/model/token_storage.dart';
 
 import 'package:threddit_clone/models/subreddit.dart';
+import 'package:threddit_clone/theme/text_styles.dart';
 
 class SearchCommunityWidget extends ConsumerStatefulWidget {
   final String searchText;
@@ -25,6 +27,7 @@ class _SearchCommunityScreenState extends ConsumerState<SearchCommunityWidget> {
   bool _fetching = true;
   bool _fetchingFinish = true;
   String? userId;
+  final List<String> tabs = ['Best', 'Hot', 'New', 'Top'];
 
   @override
   void initState() {
@@ -47,9 +50,10 @@ class _SearchCommunityScreenState extends ConsumerState<SearchCommunityWidget> {
   }
 
   Future _fetchSubreddits() async {
-    print("ALOOOOOOOOOOOOOOOOOOOOO1111");
 
-    final response = await searchTest(widget.searchText, _currentPage);
+    String searchValue = ref.read(selectedSortProvider);
+    final response =
+        await searchTest(widget.searchText, _currentPage, sorting: searchValue);
 
     final SearchModel results = response;
     print(results.subreddits.length);
@@ -80,6 +84,15 @@ class _SearchCommunityScreenState extends ConsumerState<SearchCommunityWidget> {
 
   @override
   Widget build(BuildContext context) {
+    ref.listen(selectedSortProvider, (previous, next) {
+      setState(() {
+        _subreddits.clear();
+        _currentPage = 1;
+        _fetching = true;
+        _fetchingFinish = true;
+      });
+      _fetchSubreddits();
+    });
     if (_subreddits.isEmpty) {
       if (_fetching) {
         return Center(
@@ -100,40 +113,66 @@ class _SearchCommunityScreenState extends ConsumerState<SearchCommunityWidget> {
         );
       }
     } else {
-      return ListView.builder(
-        controller: _scrollController,
-        itemCount: _subreddits.length + 1,
-        itemBuilder: (context, index) {
-          if (index < _subreddits.length) {
-            return CommunityUnit(
-              subreddit: _subreddits[index],
-              userID: userId!,
-            );
-          } else {
-            return _fetchingFinish
-                ? SizedBox(
-                    height: 75.h,
-                    width: 75.w,
-                    child: Lottie.asset(
-                      'assets/animation/loading2.json',
-                      repeat: true,
-                    ),
-                  )
-                : SizedBox(
-                    child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Center(
-                      child: Text(
-                        'No more communities available.',
-                        style: TextStyle(
-                          fontSize: 20.sp,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ));
-          }
-        },
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          DropdownMenu<String>(
+            inputDecorationTheme: const InputDecorationTheme(
+              border: InputBorder.none,
+            ),
+            textStyle: AppTextStyles.primaryTextStyle,
+            dropdownMenuEntries:
+                tabs.map<DropdownMenuEntry<String>>((String string) {
+              return DropdownMenuEntry(value: string, label: string);
+            }).toList(),
+            width: 150.w,
+            initialSelection: ref.read(selectedSortProvider),
+            onSelected: (String? value) {
+              ref.read(selectedSortProvider.notifier).state = value!;
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('Changed to tab $value'),
+                duration: Durations.short1,
+              ));
+            },
+          ),
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount: _subreddits.length + 1,
+              itemBuilder: (context, index) {
+                if (index < _subreddits.length) {
+                  return CommunityUnit(
+                    subreddit: _subreddits[index],
+                    userID: userId!,
+                  );
+                } else {
+                  return _fetchingFinish
+                      ? SizedBox(
+                          height: 75.h,
+                          width: 75.w,
+                          child: Lottie.asset(
+                            'assets/animation/loading2.json',
+                            repeat: true,
+                          ),
+                        )
+                      : SizedBox(
+                          child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Text(
+                              'No more communities available.',
+                              style: TextStyle(
+                                fontSize: 20.sp,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ));
+                }
+              },
+            ),
+          ),
+        ],
       );
     }
   }
